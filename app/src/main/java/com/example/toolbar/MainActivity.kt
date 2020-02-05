88019f2aad864a862ecab729aa9ace32e11b551a
Jonathan Ramirez <holamundo@gmail.com>
2020-02-05 14:43:43 -0500

    subida de TolBar

---
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.bfav -> {
+                Toast.makeText(this, "Elemento agregado como favorito", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> {return super.onOptionsItemSelected(item)}
+        }
+
+    }
+}
